a0405ba2d5b3b2a7fef09ec135714a5c0d5d9eb8
lahiruSandaruwan20 <lahiru.20210390@iit.ac.lk>
2025-02-26 14:04:53 +0530

    Project initialise.

---
+++ src/test/java/com/uro_alert/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uro_alert.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
